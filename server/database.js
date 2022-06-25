aa06ea4bcf1a9e5c746555a00c5e74870d320eb8
Keyla Koizumi Nishimura <kkoizuminishimura@gmail.com>
2022-06-25 12:08:19 -0700

    set up database

---
+++ server/database.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const url = 'postgres://twkezsld:ODdiyibD-s-GJ1AWcIyaQHCrxapv6kzi@heffalump.db.elephantsql.com/twkezsld';
+
+const pool = new Pool({
+  connectionString: url
+})
+
+module.exports = {
+  query: (text, params, callback) => pool.query(text, params, calback),
+}
