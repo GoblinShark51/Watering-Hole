5b17847aadeea9b3a4ba8acc70711ca3f3e3fc35
Jared Veltsos <veltsos.jared@gmail.com>
2022-06-26 01:00:34 -1000

    Question component, react-router config, set up initial state and request logic
    Made some miner edits to previous code, added dynamic route for requesting individual question posts, added useEffect hook to place our fetch request

--- client/index.js
+++ client/index.js
@@ -1,9 +1,28 @@
+import ReactDOM from 'react-dom/client';
 import React from 'react';
-import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from './App.jsx'
+import App from './App.jsx';
+import QuestionList from './components/questionList.jsx';
+import Question from './components/questionComponents/Question.jsx'
 
-render(
-    <App/>,
-    document.getElementById('root')
-  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/questions' element={<QuestionList />} />
+      <Route path='/:questionId' element={<Question />}/>
+      <Route path='/' element={<App />}>
+        {/* <Route path='invoices' element={<Invoices />} /> */}
+      </Route>
+      <Route
+        path='*'
+        element={
+          <main style={{ padding: '1rem' }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
+);
