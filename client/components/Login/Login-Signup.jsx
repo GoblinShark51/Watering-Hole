0f9849ba90564d0b52b47f2fa6057a0f82e11ec1
Jared Veltsos <veltsos.jared@gmail.com>
2022-06-27 12:19:49 -1000

    only re route if no error from db upon signin/login

--- client/components/Login/Login-Signup.jsx
+++ client/components/Login/Login-Signup.jsx
@@ -12,61 +12,62 @@ function LoginSignup() {
   let ENDPOINT;
 
   // changing the value of the route to send request to depending on state
-  hasAccount ? ENDPOINT = '/login' : ENDPOINT = '/signup';
+  hasAccount ? (ENDPOINT = '/login') : (ENDPOINT = '/signup');
 
-const handleClick = (event) => {
-  const username = event.target.username.value
-  const password = event.target.password.value
+  const handleClick = (event) => {
+    const username = event.target.username.value;
+    const password = event.target.password.value;
 
-  console.log('Verified: got username and password');
-  // realized this has to be a POST request no matter what, you can send info in the body of the request BUT
-  // the internet says that it's bad practice and you should send a post request if you want to put info in the req.body
-  // send a different request depending on state
-  fetch(ENDPOINT, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      username,
-      password
+    console.log('Verified: got username and password');
+    // realized this has to be a POST request no matter what, you can send info in the body of the request BUT
+    // the internet says that it's bad practice and you should send a post request if you want to put info in the req.body
+    // send a different request depending on state
+    fetch(ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
     })
-  }).then((response) => {
-    console.log('Verified: recieved response from server.');
-    return response.json();
-  })
-  .then((data) => {
-    // do something
-    setUsername({
-      username: data.username,
-      id: data._id
-    })
+      .then((response) => {
+        console.log('Verified: recieved response from server.');
+        return response.json();
+      })
+      .then((data) => {
+        // do something
+        setUsername({
+          username: data.username,
+          id: data._id,
+        });
 
-    console.log('Logged in with: ', data);
-
-    return navigate('/questions', {replace: true}), [navigate];
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-}
+        if (username) navigate('/questions', { replace: true }), [navigate];
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     // using fragment as a wrapper instead of a div as to not have ajacent elements
     <React.Fragment>
       Welcome to the WateringHole
       <div>
-        <form onSubmit={(event) => {
-          event.preventDefault();
-          handleClick(event)
-        }}>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleClick(event);
+          }}
+        >
           <input type='text' name='username' />
           <br />
           <br />
           <input type='text' name='password' />
           <br />
           {/* this is just here because I can't figure out how to wrap a submite button with the link tag */}
-          <input type="submit" name="" id="" />
+          <input type='submit' name='' id='' />
         </form>
         {username}
       </div>
