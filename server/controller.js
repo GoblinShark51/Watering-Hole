2b405224bf66bd5b6eb88045b0266ca711b61319
Tran McFarland Nguyen <73681436+Tranimal-N@users.noreply.github.com>
2022-06-27 10:51:14 -0700

    made changes to server and controller

--- server/controller.js
+++ server/controller.js
@@ -6,15 +6,23 @@ controller = {};
 controller.createUser = (req, res, next) => {
     const {username, password} = req.body;
 
+    console.log(req.body);
+
     const query = `INSERT INTO users
     (username, password)
-    VALUES ('${username}', '${password}')`
+    VALUES ('${username}', '${password}') RETURNING _id, username;`
+    //VALUES ('${username}', '${password}')`
 
     dataBase.query(query)
+        
         .then(data => {
-            res.locals.createUser = data.rows.username;
-            next()
+            console.log(data)
+            data.rows;
         })
+        .then(data => {
+            res.locals.createUser = data[0];
+            next();
+        })      
         .catch(err => console.log(err))
     next();
 };
@@ -26,7 +34,10 @@ controller.getUser = (req, res, next) => {
 
     const query = `SELECT username, password
     FROM users
-    WHERE username = '${username}' AND password = '${password}'`
+    WHERE username = '${username}' AND password = '${password};'`
+
+    // database.query(query, [username, password])
+    //`SELECT username, password FROM users WHERE username=$1 AND password=$2
 
     dataBase.query(query)
         .then(data => {
@@ -40,15 +51,16 @@ controller.getUser = (req, res, next) => {
 // GET questions list (questions page): function to get list of questions stored in database
 controller.getQuestions = (req, res, next) => {
     // use inner join
+    const{} = re.body;
     // [{id: id, questionTitle: title, questionAuthor: theirname, timestamp: time}]
-    const query = `SELECT questions.id, questions.questionTitle, questions.timestamp, users.username
+    const query = `SELECT questions.id, questions.title, questions.timestamp, users.username
     FROM questions
     INNER JOIN users
-    ON questions.id = users.d_author`
+    ON questions.id_author = users.id;`
 
     dataBase.query(query)
         .then(data => {
-            res.locals.getQuestions = data;
+            res.locals.getQuestions = [data.rows];
             next();
         })
         .catch(err => console.log(err))
@@ -56,12 +68,29 @@ controller.getQuestions = (req, res, next) => {
 };
 
 // GET question (with comment data)
+/*
+* GET question. Client will use /:id param where id is the ID of the question
+{
+  id: id
+  questionTitle: title,
+  questionAuthor: theirname,
+  questionContent: content,
+  timestamp: time,
+  comments: [{id: id, author: theirname, content: theirtext, timestamp: time}]
+}
+*/
 controller.getQuestionsWithComments = (req, res, next) => {
-    const query = ``
+    const {id} = req.body;
+
+    const query = `SELECT q.id, q.title, q.q_content, q.times_tamp, 
+    u.author AS question_author,
+    comments.id, comments.author, comments.content, comments.timestamp
+    FROM questions q
+    INNER JOIN users u ON q.id_author = u.id
+    INNER JOIN comments
+    ON `
 
 }
-
-
 
 
 // GET user info: function to get user profile from database
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const {controller} = require('./controller');
+const controller = require('./controller');
 
 app.use('/', express.static(path.join(__dirname, '../build')));
 
